df09721fc967812d59becfd6533b6250b2c84f06
ANOOP <anoopjaiswal903@gmail.com>
2024-04-21 21:07:39 +0530

    Project setup and Tailwind configuration

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+function Navbar() {
+  return (
+    <>
+      <div>
+        <GiHamburgerMenu />
+      </div>
+      <div></div>
+      <div></div>
+    </>
+  );
+}
+
+export default Navbar;
